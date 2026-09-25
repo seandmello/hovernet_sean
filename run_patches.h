0fb8e6dac8bdca888043001da2d3ccf597dd7b58
Sean <seandmello2002@gmail.com>
2026-09-25 15:00:33 -0400

    asd

---
+++ run_patches.h
@@ -0,0 +1,3 @@
+python run_infer.py --gpu=0 --nr_types=5 --type_info_path=type_info.json
+  --model_path=/cluster/home/t140585uhn/hovernet_fast_monusac_type_tf2pytorch.tar --model_mode=fast --batch_size=32 tile
+  --input_dir=/cluster/home/t140585uhn/patches/ --output_dir=/cluster/home/t140585uhn/output/
